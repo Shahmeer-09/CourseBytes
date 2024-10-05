2afeaca626deb28fff3529e77d2d55e940669116
Shahmeer-09 <shahmeerweb@gmail.com>
2024-10-05 10:13:04 +0500

    "deleted custom error form lib"

--- lib/CustomError.ts
+++
@@ -1,9 +0,0 @@
-
-
-export const MyRsponse  = (msg:string, success:boolean, data:any)=>{
-
-   return {message:msg, success:success, data:data}
-}
-export const MyError  = (error:Error)=>{
-   return {message:error.message}
-}
